0342e44d20f431a54bc4c3b98fba79bf4b9012e5
Your Name <ezeqduarte01@gmail.com>
2022-11-02 16:22:32 -0300

    carpetas

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import logo from './logo.svg';
-import './App.css';
 import users from './data/registredUsers';
 import activities from './data/activities';
 import places from './data/cities';
@@ -13,22 +12,7 @@ function App() {
   console.log(events)
   
   return (    
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <></>
   );
 }
 
