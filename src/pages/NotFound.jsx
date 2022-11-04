d1aff4f58ac13b46433aac1d605485c1478090f3
matiasecharri <matiasecharri@hotmail.com>
2022-11-04 01:09:52 -0300

    algunos detalles

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function NotFound() {
+  return (
+    <div>
+
+<H2> We sorry! this page does not exist or we could not find it... </H2>
+<CallToAction text="CITIES" rute="/cities"/>
+<CallToAction text="HOTELS" rute="/hotels"/>
+
+    </div>
+  )
+}
+
+
+
