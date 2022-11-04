c150045926d52e274fd5b59318d4ef4d7fa1715a
matiasecharri <matiasecharri@hotmail.com>
2022-11-04 14:29:20 -0300

    se empieza con la pag para crear nuevos hoteles

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Main from "./layouts/Main";
 import SignUp from "./pages/signup/SignUp";
 import NotFound from "./pages/NotFound";
 import LogIn from "./pages/login/LogIn"
+import NewHotel from "./pages/newHotel/NewHotel";
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
         <Route path="/signup" element={<SignUp/>} />
         <Route path="/login" element={<LogIn/>} />
         <Route path="*" element={<NotFound/>} />
+        <Route path="newhotel" element={<NewHotel/>} />
       </Routes>
 
     </Main>
