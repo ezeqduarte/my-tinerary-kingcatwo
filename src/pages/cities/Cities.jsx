6e0167cff73daab92e34762ff56e0d9fe95b6326
Your Name <ezeqduarte01@gmail.com>
2022-11-06 15:54:28 -0300

    cities terminada

--- src/pages/cities/Cities.jsx
+++ src/pages/cities/Cities.jsx
@@ -1,64 +1,87 @@
-import React, { useRef, useState } from 'react'
-import CardCity from '../../components/CardCity';
-import GoTo from '../../components/GoTo';
-import Label from '../../components/Label';
-import places from '../../data/cities';
-import '../cities/cities.css';
+import React, { useRef, useState, useEffect } from "react";
+import CardCity from "../../components/CardCity";
+import GoTo from "../../components/GoTo";
+import Label from "../../components/Label";
+import places from "../../data/cities";
+import "../cities/cities.css";
+
+const initialChecboxsValues = {
+  Asia: false,
+  America: false,
+  Africa: false,
+  Europe: false,
+};
 
 export default function Cities() {
 
-    let continents = [...new Set ([...places].map(place=>place.continent))]
+  let continents = [...new Set([...places].map((place) => place.continent))];
 
-    let [inputText, setInputText] = useState('');
-    let [checkboxs, setCheckboxs] = useState([])
+  let [inputText, setInputText] = useState("");
 
-    let renderInput = (e) => {
+  let [checkboxs, setCheckboxs] = useState(initialChecboxsValues);
 
-        setInputText(e.target.value);
-        console.log(e.target.value);
-
-    }
-
+  let renderInput = (e) => {
 
     
+    setInputText(e.target.value);
+    console.log(e.target.value);
+
+  };
+
+  const checkboxsCheckeds = (e) => {
+    setCheckboxs((checkboxs) => ({
+      ...checkboxs,
+      [e.target.value]: e.target.checked,
+    }));
+    console.log(checkboxs);
+  };
+
+  const form = useRef();
+
 
 
   return (
-        <>
-    
+    <>
+      <div className="headerCities">
+        <h2>
+          Meet our most popular locations<span className="rojo">.</span>
+        </h2>
+        <GoTo anchor="#cities"></GoTo>
+      </div>
 
-        <div className='headerCities'>
-            <h2>Meet our most popular locations<span className="rojo">.</span></h2>
-            <GoTo anchor="#cities"></GoTo>
+      <div className="bodyCities" id="cities">
+        <h2>
+          Cities<span className="rojo">.</span>
+        </h2>
+        <div className="citiesPhrase">
+          <p>The most populars cities, visited by our travelers...</p>
         </div>
 
-        <div className='bodyCities' id='cities'>
-            <h2>Cities<span className="rojo">.</span></h2>
-            <div className='citiesPhrase'>
-                <p>The most populars cities, visited by our travelers...</p>
-            </div>
-            <form className='inputs'>
+        <form className="inputs" ref={form} >
+          <fieldset className="checkboxs" onChange={checkboxsCheckeds} >
+            {continents.map((continent) => (
+              <Label continent={continent} key={continent}></Label>
+            ))}
+          </fieldset>
 
-                <fieldset className='checkboxs' >
-                    {continents.map(continent=><Label continent={continent}></Label>)}
-                </fieldset>
-
-                <fieldset>
-                    <label>Search for name of city
-                        <input type="text" className='searchForText' onChange={(e)=> renderInput(e)} value={inputText} />
-                    </label>
-                </fieldset>
-                    
-            </form>
-            <div className='mainCities'>                
-            
-                {places.map(place=><CardCity city={place}/>)}
-                
-            </div>
+          <fieldset>
+            <label>
+              Search for name of city
+              <input
+                type="text"
+                className="searchForText"
+                onChange={renderInput}
+                value={inputText}
+              />
+            </label>
+          </fieldset>
+        </form>
+        <div className="mainCities">
+          {places.map((place) => (
+            <CardCity city={place} key={place.id} />
+          ))}
         </div>
-
-
-        </>
-
-  )
+      </div>
+    </>
+  );
 }
