56e9ae684137fd83213861337abd8d49837dc0ce
Your Name <ezeqduarte01@gmail.com>
2022-11-13 03:59:33 -0300

    checkboxs por base de datos y all cities con peticion a bbdd

--- src/pages/cities/Cities.jsx
+++ src/pages/cities/Cities.jsx
@@ -1,8 +1,9 @@
+import axios from "axios";
 import React, { useRef, useState, useEffect } from "react";
 import CardCity from "../../components/CardCity";
 import GoTo from "../../components/GoTo";
 import Label from "../../components/Label";
-import places from "../../data/cities";
+
 import "../cities/cities.css";
 
 const initialChecboxsValues = {
@@ -12,20 +13,25 @@ const initialChecboxsValues = {
   Europe: false,
 };
 
-export default function Cities() {
+export default function Cities() { 
 
-  let continents = [...new Set([...places].map((place) => place.continent))];
-
+  let [allCities, setAllCities] = useState([]);
   let [inputText, setInputText] = useState("");
+  let [checkboxs, setCheckboxs] = useState(initialChecboxsValues);  
 
-  let [checkboxs, setCheckboxs] = useState(initialChecboxsValues);
+  useEffect(() => {
+    axios
+      .get("http://localhost:8000/api/cities/")
+      .then((response) => setAllCities(response.data.cities));
+  }, []);
+
+  let continents = [...new Set([...allCities].map((place) => place.continent))];
+
+
 
   let renderInput = (e) => {
-
-    
     setInputText(e.target.value);
     console.log(e.target.value);
-
   };
 
   const checkboxsCheckeds = (e) => {
@@ -33,14 +39,10 @@ export default function Cities() {
       ...checkboxs,
       [e.target.value]: e.target.checked,
     }));
-    
   };
-  
 
   console.log(checkboxs);
   const form = useRef();
-
-
 
   return (
     <>
@@ -59,8 +61,8 @@ export default function Cities() {
           <p>The most populars cities, visited by our travelers...</p>
         </div>
 
-        <form className="inputs" ref={form} >
-          <fieldset className="checkboxs" onChange={checkboxsCheckeds} >
+        <form className="inputs" ref={form}>
+          <fieldset className="checkboxs" onChange={checkboxsCheckeds}>
             {continents.map((continent) => (
               <Label continent={continent} key={continent}></Label>
             ))}
@@ -79,8 +81,8 @@ export default function Cities() {
           </fieldset>
         </form>
         <div className="mainCities">
-          {places.map((place) => (
-            <CardCity city={place} key={place.id} />
+          {allCities.map((place) => (
+            <CardCity city={place} key={place._id} />
           ))}
         </div>
       </div>
