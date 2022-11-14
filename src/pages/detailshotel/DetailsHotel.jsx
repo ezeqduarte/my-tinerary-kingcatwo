9fbe0129680a346f529eb7b0a98dae54fe6641a8
matiasecharri <matiasecharri@hotmail.com>
2022-11-14 20:43:47 -0300

    ya es posible renderizar

--- src/pages/detailshotel/DetailsHotel.jsx
+++ src/pages/detailshotel/DetailsHotel.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+import { useEffect } from "react";
 import { useParams } from "react-router";
 import CardDetailsHotel from "../../components/CardDetailsHotel";
 import ItineraryEvents from "../../components/ItineraryEvents";
@@ -8,8 +10,14 @@ import "../detailshotel/detailshotels.css";
 export default function () {
 
 const {id} = useParams()
-let itineraryEventsX = events.filter(event=>event.hotelId === id)
+// let itineraryEventsX = events.filter(event=>event.hotelId === id)
+let [itineraryEventsX, setItineraryEventsX] = useState([])
+useEffect(()=> {
 
+axios.get(`http://localhost:8000/api/shows?hotelId=${id}`)
+.then ((Response) => setItineraryEventsX(Response.data.searched))
+
+},[])
 
   return (
     <div>
@@ -22,6 +30,9 @@ let itineraryEventsX = events.filter(event=>event.hotelId === id)
           Events<span className="rojo">.</span>{" "}
         </h2>
         <div className="eventsfromhotels">
+
+
+
 
             {itineraryEventsX.map(event=><ItineraryEvents object={event} key={event.name}> </ItineraryEvents>)}        
 
