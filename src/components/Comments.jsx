9be886a92ae65de370c819fb6d45e808acfeccd3
matiasecharri <matiasecharri@hotmail.com>
2022-11-30 20:21:36 -0300

    Se agregaron mediaqueris para detailscity

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,14 +1,34 @@
-import React from 'react'
+import React from "react";
 
 export default function Comments() {
   return (
-    <div className='comments100'>
-<div className='photo100'></div>
-<div>
-<p className='parrafo101'>John Doe</p>
-<p className='parrafo100'>"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rem veniam odio sunt magni repudiandae illum suscipit, repellat sit ea praesentium magnam."</p>
-    </div>
-    </div>
     
-  )
+    <div className="comments100">
+      
+      
+      <div className="photo100">
+      
+      </div>
+
+      <p className="parrafo101">John Doe</p>
+      <p className="parrafo100">
+     
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rem veniam
+        odio sunt magni repudiandae illum suscipit, repellat sit ea praesentium
+        magnam."
+      </p>
+      <div className="iconitos100">
+        <img
+          src="https://img.icons8.com/ios-glyphs/30/CCC6C6/pencil-tip.png"
+          alt=""
+          srcset=""
+        />
+        <img
+          src="https://img.icons8.com/ios-glyphs/30/CCC6C6/multiply.png"
+          alt=""
+          srcset=""
+        />
+      </div>
+    </div>
+  );
 }
