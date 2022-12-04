f6945fbfa6a46df3ddaeef601e8c35d6cd44a7fe
Your Name <ezeqduarte01@gmail.com>
2022-12-04 06:57:16 -0300

    arreglo en comentarios y reacciones de shows

--- src/components/MyReactions.jsx
+++ src/components/MyReactions.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import reactionsActions from "../redux/actions/reactionsActions";
+import ReactionsUserShows from "../components/ReactionsUserShows";
 import ReactionUser from "./ReactionUser";
 
 export default function MyReactions() {
@@ -22,7 +23,13 @@ export default function MyReactions() {
   const reactionsOfTineraries = allReactionsOfUser.filter(
     (reaction) => reaction.itineraryId
   );
-  console.log(reactionsOfTineraries);
+  
+
+  const reactionsOfShows = allReactionsOfUser.filter(
+    (reaction) => reaction.showId
+  );
+
+ 
 
   return (
     <>
@@ -32,12 +39,25 @@ export default function MyReactions() {
       <div className="ContainerReactionUser999">
         {reactionsOfTineraries.length > 0 ? (
           reactionsOfTineraries.map((reaction) => (
-            <ReactionUser reaction={reaction} />
+            <ReactionUser key={reaction._id} reaction={reaction} />
           ))
         ) : (
           <h2>You dont have reactions of Tineraries</h2>
         )}
       </div>
+      <h3>
+        Your reactions of shows are here<span className="rojo">.</span>
+      </h3>
+      <div className="ContainerReactionUser999">
+        {reactionsOfShows.length > 0 ? (
+          reactionsOfShows.map((reaction) => (
+            <ReactionsUserShows key={reaction._id} reaction={reaction} />
+          ))
+        ) : (
+          <h2>You dont have reactions of Tineraries</h2>
+        )}
+      </div>
+      
     </>
   );
 }
---
+++ src/components/ReactionsUserShows.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import reactionsActions from "../redux/actions/reactionsActions";
+import Swal from "sweetalert2/dist/sweetalert2.js";
+
+export default function ReactionsUserShows({ reaction }) {
+  const { token } = useSelector((store) => store.userReducer);
+  let { _id, showId } = reaction;
+  const dispatch = useDispatch();
+  const { deleteReaction } = reactionsActions;
+
+  const deleteReactionFunction = (e) => {
+    e.preventDefault();
+
+    Swal.fire({
+      title: `Are you sure to delete this reaction in ${showId.name}?`,
+      imageUrl: "https://img.icons8.com/ios-glyphs/120/000000/break.png",
+      width: "25rem",
+      padding: "2rem",
+      showCancelButton: true,
+      confirmButtonColor: "#ff3648",
+      cancelButtonColor: "#5e5b5b",
+      confirmButtonText: "Yes, delete it!",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        await dispatch(deleteReaction({ token, id: _id }));
+        Swal.fire({
+          title: "The reaction has deleted",
+          imageUrl: "https://img.icons8.com/sf-regular/120/null/ok.png",
+          width: "25rem",
+          padding: "2rem",
+        });
+      }
+    });
+  };
+
+  return (
+    <div className="ReactionUser999">
+      <img src={reaction?.showId.photo} alt="" />
+      <div className="infoReaction99">
+        <p className="negrita99">{reaction?.showId.name}</p>
+
+        <p>Your reaction in {reaction?.showId.name} was </p>
+
+        <img src={reaction.icon} alt="" />
+      </div>
+      <button
+        onClick={deleteReactionFunction}
+        className="negrita99 btn-details"
+      >
+        Delete
+      </button>
+    </div>
+  );
+}
