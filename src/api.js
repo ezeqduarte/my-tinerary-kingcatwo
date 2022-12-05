d40f066359cd9a2efce511b2978030dea856f60f
Your Name <ezeqduarte01@gmail.com>
2022-12-05 13:26:46 -0300

    sin back

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 const API = "http://localhost:8000/api/"
 
-if(process.env.NODE_ENV ==="production"){
+/* if(process.env.NODE_ENV ==="production"){
     API = "https://back-kingcatwo-production.up.railway.app/api/"
-}
+} */
 
 export default API
