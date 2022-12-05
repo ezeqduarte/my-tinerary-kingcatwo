d44f08b340e177ed6345fb9a05855e2ff785ddab
Your Name <ezeqduarte01@gmail.com>
2022-12-05 12:07:21 -0300

    cambio en api para probar host

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-const API = "http://localhost:8000/api/"
+const API = "https://back-kingcatwo-production.up.railway.app/api/"
 
 if(process.env.NODE_ENV ==="production"){
     API = process.env.REACT_APP_URL
