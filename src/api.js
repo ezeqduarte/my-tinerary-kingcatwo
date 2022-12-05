5b1a0492982be00279b9ccd64519476696631db3
Your Name <ezeqduarte01@gmail.com>
2022-12-05 13:18:08 -0300

    cambio api

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 const API = "http://localhost:8000/api/"
 
 if(process.env.NODE_ENV ==="production"){
-    API = process.env.REACT_APP_URL
+    API = "https://back-kingcatwo-production.up.railway.app/api/"
 }
 
 export default API
